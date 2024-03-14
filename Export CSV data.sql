a6d8e2c5647ca01c5c694cb80edeb8338ed16154
mehdi-rgp <mehdi.rezagholipour@gmail.com>
2024-03-14 16:11:06 +0000

    Add files via upload

--- Export CSV data.sql
+++ Export CSV data.sql
@@ -1,9 +1,15 @@
-SELECT  'ride_id', 'rideable_type', 'started_at', 'ended_at', 'start_station_id', 'end_station_id', 
-'member_casual', 'start_station_name', 'start_lat', 'start_lng', 'end_station_name', 'end_lat', 'end_lng'
+SELECT  
+	'ride_id',
+    'rideable_type',
+    'start_station_id',
+    'end_station_id',
+    'started_at',
+    'ended_at',
+    'member_casual'
 UNION 
 SELECT *
-FROM bikeshare.tripdata2023_cleaned
-INTO OUTFILE 'tripdata2023_cleaned.csv'
+FROM tripdata2023v6
+INTO OUTFILE 'tripdata2023_cleanedv7.csv'
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n';
